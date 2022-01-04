0645fbecc43498f134f1bed3aa417a21df9b8db3
Rickard Sundin <rickard.sundin@postkodlotteriet.se>
2022-01-04 14:12:33 +0100

    Upgrade dependencies

--- src/main/java/se/novamedia/service/leads/jdbi/LeadsDao.java
+++ src/main/java/se/novamedia/service/leads/jdbi/LeadsDao.java
@@ -7,7 +7,7 @@ import se.novamedia.service.leads.api.LeadCreationRequest;
 
 public interface LeadsDao {
 
-    @SqlUpdate("INSERT INTO leads (lead_id, ssn, first_name, last_name, email, created_at) VALUES (lead_id_seq.nextval,:ssn, :firstName, :lastName, :email, now())")
+    @SqlUpdate("INSERT INTO leads (lead_id, ssn, first_name, last_name, email, created_at) VALUES (nextval('lead_id_seq'),:ssn, :firstName, :lastName, :email, now())")
     @GetGeneratedKeys("lead_id")
     long insertLead(@BindFields LeadCreationRequest request);
 }
